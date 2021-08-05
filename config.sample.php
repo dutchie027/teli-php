f1faeb92c50e895d094ce918989ce744adb21a5e
dutchie027 <justin@227volts.com>
2021-08-04 22:03:33 -0400

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -1,8 +1,9 @@
 <?php
 
 # Edit the settings in this file and rename it config.php (remove the .sample)
-# it's also suggested to possibly move this file outside the scope of the webserver
-# and reference it with an absolute path like /opt/configs/teli.php
+# it's also suggested to move this file outside the scope of the webserver
+# and reference it with an absolute path like 
+# require_once '/opt/configs/teli.php';
 
 # Find these two values at https://control.teli.net/user/account
 define ('TELI_API_TOKEN', '');
